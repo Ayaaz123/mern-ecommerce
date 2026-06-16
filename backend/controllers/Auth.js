1f47dd0e228049fb3d7cad5eab36feb5fa870abf
Abdul Ayaaz <110470321+Ayaaz123@users.noreply.github.com>
2026-06-16 23:20:35 +0530

    Update Auth.js

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -32,8 +32,8 @@ exports.signup=async(req,res)=>{
 
         // sending jwt token in the response cookies
         res.cookie('token',token,{
-            sameSite:process.env.PRODUCTION==='true'?"None":'Lax',
-            maxAge:new Date(Date.now() + (parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000))),
+            sameSite: process.env.PRODUCTION === 'true' ? 'none' : 'Lax',
+            maxAge: 30 * 24 * 60 * 60 * 1000,
             httpOnly:true,
             secure:process.env.PRODUCTION==='true'?true:false
         })
