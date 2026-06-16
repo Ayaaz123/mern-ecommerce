9ce0aaaaef4c6100d29a1ff4452d1f060bc8fa53
Abdul Ayaaz <110470321+Ayaaz123@users.noreply.github.com>
2026-06-16 23:36:41 +0530

    Update Emails.js

--- backend/utils/Emails.js
+++ backend/utils/Emails.js
@@ -2,12 +2,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for port 465 SSL connection which bypasses Render's firewall
+  port: 587,
+  secure: false, // false for port 587 (it upgrades to secure connection automatically)
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false // This stops Render from blocking the secure certificate handshake
+  }
 });
 
 exports.sendMail = async (receiverEmail, subject, body) => {
